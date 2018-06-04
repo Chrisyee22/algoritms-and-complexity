e6a5a6b033ec80c78a46df24507d1436f4f39fc6
Chris <chris.anthony.yee@gmail.com>
2018-06-03 19:06:57 -0700

    intro

---
+++ fibonacci_iterative.rb
@@ -0,0 +1,22 @@
+def fib(n)
+  if n == 0
+    0
+  elsif n == 1
+    1
+  elsif n == 2
+    1
+  else
+    fib(n-1) + fib(n-2)
+  end
+end
+
+puts fib(0)
+ puts fib(1)
+ puts fib(2)
+ puts fib(3)
+ puts fib(4)
+ puts fib(5)
+ puts fib(6)
+ puts fib(7)
+ puts fib(8)
+ puts fib(9)
