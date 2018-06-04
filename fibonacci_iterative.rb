d4161fda7703ab601c9d017fea7445ee32656f45
Chris <chris.anthony.yee@gmail.com>
2018-06-04 14:27:13 -0700

    revised fib

--- fibonacci_iterative.rb
+++ fibonacci_iterative.rb
@@ -1,22 +1,22 @@
 def fib(n)
-  if n == 0
-    0
-  elsif n == 1
-    1
-  elsif n == 2
-    1
-  else
-    fib(n-1) + fib(n-2)
+  fib_0 = 0
+  fib_1 = 1
+
+  (0..(n-1)).each do
+    temp = fib_0
+    fib_0 = fib_1
+    fib_1 = temp + fib_1
   end
+  return fib_1
 end
 
 puts fib(0)
- puts fib(1)
- puts fib(2)
- puts fib(3)
- puts fib(4)
- puts fib(5)
- puts fib(6)
- puts fib(7)
- puts fib(8)
- puts fib(9)
+puts fib(1)
+puts fib(2)
+puts fib(3)
+puts fib(4)
+puts fib(5)
+puts fib(6)
+puts fib(7)
+puts fib(8)
+puts fib(9)
